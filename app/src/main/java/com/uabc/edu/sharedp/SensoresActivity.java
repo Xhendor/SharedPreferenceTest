c44f7ac8dd9ca5b704e599ddd4a4013b0f0a173a
xhendor <rosendorsc@gmail.com>
2020-01-14 20:54:44 -0800

    Se agrego ejemplo Sensores

--- app/src/main/java/com/uabc/edu/sharedp/SensoresActivity.java
+++ app/src/main/java/com/uabc/edu/sharedp/SensoresActivity.java
@@ -3,12 +3,20 @@ package com.uabc.edu.sharedp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 
-public class SensoresActivity extends AppCompatActivity {
+public class SensoresActivity extends AppCompatActivity implements
+        SensorEventListener {
 
+        private SensorManager sensorManager;
+        private TextView equis,ye,zeta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +32,46 @@ public class SensoresActivity extends AppCompatActivity {
             Usuario elUser=gson.fromJson(usuario,Usuario.class);
             this.setTitle("Hola: "+elUser.getNombre());
         }
+
+        sensorManager= (SensorManager) getSystemService(
+                SENSOR_SERVICE);
+
+        equis=findViewById(R.id.equis);
+        ye=findViewById(R.id.ye);
+        zeta=findViewById(R.id.zeta);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+                float z=event.values[0];
+                float x=event.values[1];
+                float y=event.values[2];
+                equis.setText(""+x);
+                ye.setText(""+y);
+                zeta.setText(""+z);
+        }
+        if(event.sensor.getType()==Sensor.TYPE_ORIENTATION){
+
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+            sensorManager.registerListener(
+                    this,sensorManager.
+                    getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                    SensorManager.SENSOR_DELAY_NORMAL);
+
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(
+                        Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
